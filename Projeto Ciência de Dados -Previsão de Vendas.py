91247e85dccecb58a125675566b6d1d1c2f73f35
Vitória Canon <98993613+Miyamizuu@users.noreply.github.com>
2022-04-11 20:01:21 -0300

    Update Projeto Ciência de Dados -Previsão de Vendas.py

---
+++ Projeto Ciência de Dados -Previsão de Vendas.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Importar a base de vendas 
+base = pd.read_excel("Investimento x Vendas.xlsx")
+
+#Exibindo as 5 primeiras linhas 
+base.head()
+
+#Visualizando de forma gráfica essas informaçõe
+plt.scatter(base["Investimento em marketing"],base["Venda Qtd"]
+plt.show()
+
+#Traçando uma reta 
+plt.scatter(base["Investimento em marketing"],base["Venda Qtd"])
+x0 = base["Investiment em marketing"][0]
+y0 = base["Venda Qtd"][0]
+x1 = base["Investimento em marketing"][6]
+y1 = base["Venda Qtd"][6]
+plt.plot([x0,x1],[y0,y1],"r")
+plt.show()
+
